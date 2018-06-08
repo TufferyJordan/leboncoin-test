687eb848b9d06cd2d153289084a147363a42275d
JordanTuffery <tufferyjordan@gmail.com>
2018-06-08 04:17:27 +0200

    add the photo details activity and changed photo to parcelable

--- android/app/src/main/java/com/jordantuffery/leboncoin/api/Photo.kt
+++ android/app/src/main/java/com/jordantuffery/leboncoin/api/Photo.kt
@@ -1,10 +1,15 @@
 package com.jordantuffery.leboncoin.api
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * POJO for the REST API
  */
+
+@Parcelize
 data class Photo(val albumId: Int,
                  val id: Int,
                  val title: String,
                  val url: String,
-                 val thumbnailUrl: String)
+                 val thumbnailUrl: String) : Parcelable
--- android/app/src/main/java/com/jordantuffery/leboncoin/presentation/photos/PhotoAdapter.kt
+++ android/app/src/main/java/com/jordantuffery/leboncoin/presentation/photos/PhotoAdapter.kt
@@ -1,5 +1,6 @@
 package com.jordantuffery.leboncoin.presentation.photos
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.jordantuffery.leboncoin.R
 import com.jordantuffery.leboncoin.api.Photo
+import com.jordantuffery.leboncoin.presentation.PhotoDetailsActivity
 import kotlinx.android.synthetic.main.item_photo_list.view.item_photo_list_image_view
 import kotlinx.android.synthetic.main.item_photo_list.view.item_photo_list_text_view
 
@@ -25,11 +27,21 @@ class PhotoAdapter(var adapterList: List<Photo>) : RecyclerView.Adapter<PhotoAda
             Glide.with(holder.rootView).load(thumbnailUrl).into(holder.imageView)
             holder.textView.text = String.format(
                     holder.rootView.context.resources.getString(R.string.item_photo_list_title), id)
+            val context = holder.rootView.context
+            holder.rootView.setOnClickListener {
+                context.startActivity(Intent(context, PhotoDetailsActivity::class.java).apply {
+                    putExtra(KEY_PHOTO_PARCELIZED, adapterList[position])
+                })
+            }
         }
     }
 
     class ViewHolder(val rootView: View) : RecyclerView.ViewHolder(rootView) {
         val imageView: ImageView = rootView.item_photo_list_image_view
         val textView: TextView = rootView.item_photo_list_text_view
+    }
+
+    companion object {
+        const val KEY_PHOTO_PARCELIZED = "KEY_PHOTO_PARCELIZED"
     }
 }
